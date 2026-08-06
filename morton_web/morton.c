bc86134b217c83b99f4aa6f8045f46d2ca3cfa68
dahnny <dahnny@yandex.ru>
2026-08-06 17:46:20 +0300

    ____

--- morton_web/morton.c
+++ morton_web/morton.c
@@ -5,13 +5,15 @@
 unsigned long morton(unsigned int a, unsigned int b)
 {
 unsigned long ans=0;
-unsigned int i,L,tmp;
-L = sizeof(unsigned int);
-while(a||b){	
+unsigned int i;
+i = sizeof(unsigned int);
+do	{
+	i--;
 	ans =((ans<<2) |(((a&1)<<1)|(b&1)));
 	a = (a>>1);
 	b = (b>>1);
 	}
+while(i);
 return ans;
 }
 
